030d1082db34876eae2183b7c0daeb7602946f18
wq <guddqs@icloud.com>
2019-12-17 09:33:04 +0800

    machine code

--- app.js
+++ app.js
@@ -13,11 +13,31 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         width: 1200,
         height: 850,
+        center: true,
         autoHideMenuBar: true,
         webPreferences: {
-            nodeIntegration: false
+            nodeIntegration: true
         }
     });
+
+    mainWindow.getMachineCode = function() {
+        const execSync = require('child_process').execSync;
+        var platform = process.platform;
+        if (platform == 'darwin') {
+            var cmd = execSync('system_profiler SPHardwareDataType | grep Serial').toString();
+            var cmdArray = cmd.replace(/\n/, '').split(':');
+            if (cmdArray && cmdArray.length >= 2) {
+                return cmdArray[1].trim();
+            }
+        } else if (platform.indexOf('win') !== -1) {
+            var cmd = execSync('wmic cpu get ProcessorId').toString();
+            var cmdArray = cmd.split('\r\n');
+            if (cmdArray && cmdArray.length >= 2) {
+                return cmdArray[1].trim();
+            }
+        }
+        return 'bugbug';
+    }
 
 
     mainWindow.loadURL('file://' + __dirname + '/index.html');
